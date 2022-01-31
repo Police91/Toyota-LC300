ece126ca0fa3a0ec65002c8ca6657499a0549fdb
Andrii Girnyi <andriy.police91@gmail.com>
2022-01-31 23:06:58 +0200

    Drag-n-Drop--Section

--- app.js
+++ app.js
@@ -1,14 +1,64 @@
-const slides = document.querySelectorAll('.slide');
-console.log (slides);
+(() => {
+    function slidesPlugin(activeSlide = 3){
+        const slides = document.querySelectorAll('.slide');
+        console.log (slides);
+        
+        slides[activeSlide].classList.add('active')
+        
+        for (const slide of slides) {
+            slide.addEventListener('click', () => {
+                clearActiveClasses()
+                slide.classList.add('active')
+            })    
+        }
+        function clearActiveClasses() {
+            slides.forEach((slide) => {
+                slide.classList.remove('active')
+            })
+        }
+    }
+    slidesPlugin();
+})();
+//drag drop//
+(() => {
+    const item = document.querySelector('.item');
+    console.log(item);
+    const placeholders = document.querySelectorAll('.placeholder');
+    console.log(placeholders);
+    
+    item.addEventListener('dragstart', dragstart);
+    item.addEventListener('dragend', dragend);
+    
+    for (const placeholder of placeholders){
+        placeholder.addEventListener('dragover', dragover)
+        placeholder.addEventListener('dragenter', dragenter)
+        placeholder.addEventListener('dragleave', dragleave)
+        placeholder.addEventListener('drop', drop)
+    } 
+    
+    function dragstart(event){
+        event.target.classList.add('hold')
+        setTimeout(() => event.target.classList.add('hide'))
+    };
+    function dragend(event){
+        event.target.classList.remove('hold', 'hide')
+    };
+    
+    function dragover(event){
+        event.preventDefault()
+    }
+    
+    function dragenter(event){
+        event.target.classList.add('hovered')
+    }
+    
+    function dragleave(event){
+        event.target.classList.remove('hovered')
+    }
+    
+    function drop(event){ 
+        event.target.append(item)
+        event.target.classList.remove('hovered')
+    }    
+})();
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
-    })    
-}
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
