3e1770509abee26abc874a50b3a91620a8d6909f
Andrii Girnyi <andriy.police91@gmail.com>
2022-02-02 22:48:33 +0200

    slider color

--- app.js
+++ app.js
@@ -61,4 +61,49 @@
         event.target.classList.remove('hovered')
     }    
 })();
+(() => {
+const upBtn = document.querySelector('.up-button');
 
+const downBtn = document.querySelector('.down-button');
+
+const sidebar = document.querySelector('.sidebar');
+
+const mainSlide = document.querySelector('.main-slide');
+
+const slidesCount = mainSlide.querySelectorAll('div').length;
+
+const container = document.querySelector('.container');
+
+const height = container.clientHeight; 
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+let activeSlideIndex = 0;
+
+function changeSlide (direction) {
+    if (direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex === slidesCount)
+        {
+            activeSlideIndex = 0
+        }
+    }else if (direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1
+        }
+    }
+    
+    
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+    
+}
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up')
+})
+downBtn.addEventListener('click', () => {
+    changeSlide('down')
+})
+})();
